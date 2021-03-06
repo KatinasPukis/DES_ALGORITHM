aecd829888dcb7d79f02821c5f399b206884fdf9
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-03-06 20:56:03 +0200

    Update
    Bandymas sutvarkyti sifravima/ desifravima. sutvarkytas modos pasirinkimas. Pagrindine problema kad realizuojamas tekstas neatitinka ascii lenteles

--- DES_ALGORITHM/Form1.Designer.cs
+++ DES_ALGORITHM/Form1.Designer.cs
@@ -33,20 +33,18 @@
             this.encrypButton = new System.Windows.Forms.Button();
             this.encryptedTextResultTextBox = new System.Windows.Forms.RichTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.decryptTextBox = new System.Windows.Forms.RichTextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.decryptedTextResultBox = new System.Windows.Forms.RichTextBox();
             this.decryptButton = new System.Windows.Forms.Button();
             this.generatedKeyTextBox = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.keyToDecyptherTextBox = new System.Windows.Forms.RichTextBox();
             this.IVTextBox = new System.Windows.Forms.Label();
             this.vectorTextBox = new System.Windows.Forms.RichTextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.vectorDecryptTextBox = new System.Windows.Forms.RichTextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ECBcheckBox = new System.Windows.Forms.CheckBox();
+            this.CBCcheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // encryptTextBox
@@ -63,13 +61,13 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(68, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.Size = new System.Drawing.Size(262, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Text to encrypt:";
+            this.label1.Text = "Text to encrypt/decrypt:";
             // 
             // encrypButton
             // 
-            this.encrypButton.Location = new System.Drawing.Point(129, 360);
+            this.encrypButton.Location = new System.Drawing.Point(73, 360);
             this.encrypButton.Name = "encrypButton";
             this.encrypButton.Size = new System.Drawing.Size(116, 43);
             this.encrypButton.TabIndex = 2;
@@ -96,46 +94,9 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Encrypted text:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(427, 51);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(177, 25);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Text to decrypt:";
-            // 
-            // decryptTextBox
-            // 
-            this.decryptTextBox.Location = new System.Drawing.Point(431, 79);
-            this.decryptTextBox.Name = "decryptTextBox";
-            this.decryptTextBox.Size = new System.Drawing.Size(257, 61);
-            this.decryptTextBox.TabIndex = 5;
-            this.decryptTextBox.Text = "";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(427, 265);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(172, 25);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Decrypted text:";
-            // 
-            // decryptedTextResultBox
-            // 
-            this.decryptedTextResultBox.Location = new System.Drawing.Point(432, 293);
-            this.decryptedTextResultBox.Name = "decryptedTextResultBox";
-            this.decryptedTextResultBox.ReadOnly = true;
-            this.decryptedTextResultBox.Size = new System.Drawing.Size(257, 61);
-            this.decryptedTextResultBox.TabIndex = 7;
-            this.decryptedTextResultBox.Text = "";
-            // 
             // decryptButton
             // 
-            this.decryptButton.Location = new System.Drawing.Point(488, 360);
+            this.decryptButton.Location = new System.Drawing.Point(214, 360);
             this.decryptButton.Name = "decryptButton";
             this.decryptButton.Size = new System.Drawing.Size(116, 43);
             this.decryptButton.TabIndex = 9;
@@ -162,24 +123,6 @@
             this.label5.TabIndex = 11;
             this.label5.Text = "Key:";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(426, 143);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 25);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Key:";
-            // 
-            // keyToDecyptherTextBox
-            // 
-            this.keyToDecyptherTextBox.Location = new System.Drawing.Point(431, 171);
-            this.keyToDecyptherTextBox.Name = "keyToDecyptherTextBox";
-            this.keyToDecyptherTextBox.Size = new System.Drawing.Size(257, 28);
-            this.keyToDecyptherTextBox.TabIndex = 12;
-            this.keyToDecyptherTextBox.Text = "";
-            // 
             // IVTextBox
             // 
             this.IVTextBox.AutoSize = true;
@@ -198,53 +141,75 @@
             this.vectorTextBox.TabIndex = 14;
             this.vectorTextBox.Text = "";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(427, 206);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 25);
-            this.label7.TabIndex = 17;
-            this.label7.Text = "IV:";
-            // 
-            // vectorDecryptTextBox
-            // 
-            this.vectorDecryptTextBox.Location = new System.Drawing.Point(432, 234);
-            this.vectorDecryptTextBox.Name = "vectorDecryptTextBox";
-            this.vectorDecryptTextBox.Size = new System.Drawing.Size(257, 28);
-            this.vectorDecryptTextBox.TabIndex = 16;
-            this.vectorDecryptTextBox.Text = "";
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(263, 9);
+            this.label8.Location = new System.Drawing.Point(83, 9);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(222, 29);
             this.label8.TabIndex = 18;
             this.label8.Text = "DES ALGORITHM";
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ECBcheckBox
+            // 
+            this.ECBcheckBox.AutoSize = true;
+            this.ECBcheckBox.Location = new System.Drawing.Point(154, 423);
+            this.ECBcheckBox.Name = "ECBcheckBox";
+            this.ECBcheckBox.Size = new System.Drawing.Size(47, 17);
+            this.ECBcheckBox.TabIndex = 22;
+            this.ECBcheckBox.Text = "ECB";
+            this.ECBcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CBCcheckBox
+            // 
+            this.CBCcheckBox.AutoSize = true;
+            this.CBCcheckBox.Location = new System.Drawing.Point(202, 423);
+            this.CBCcheckBox.Name = "CBCcheckBox";
+            this.CBCcheckBox.Size = new System.Drawing.Size(47, 17);
+            this.CBCcheckBox.TabIndex = 23;
+            this.CBCcheckBox.Text = "CBC";
+            this.CBCcheckBox.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(391, 450);
+            this.Controls.Add(this.CBCcheckBox);
+            this.Controls.Add(this.ECBcheckBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.vectorDecryptTextBox);
             this.Controls.Add(this.IVTextBox);
             this.Controls.Add(this.vectorTextBox);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.keyToDecyptherTextBox);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.generatedKeyTextBox);
             this.Controls.Add(this.decryptButton);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.decryptedTextResultBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.decryptTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.encryptedTextResultTextBox);
             this.Controls.Add(this.encrypButton);
@@ -264,20 +229,18 @@
         private System.Windows.Forms.Button encrypButton;
         private System.Windows.Forms.RichTextBox encryptedTextResultTextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox decryptTextBox;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox decryptedTextResultBox;
         private System.Windows.Forms.Button decryptButton;
         private System.Windows.Forms.RichTextBox generatedKeyTextBox;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.RichTextBox keyToDecyptherTextBox;
         private System.Windows.Forms.Label IVTextBox;
         private System.Windows.Forms.RichTextBox vectorTextBox;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.RichTextBox vectorDecryptTextBox;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox ECBcheckBox;
+        private System.Windows.Forms.CheckBox CBCcheckBox;
     }
 }
 
--- DES_ALGORITHM/Form1.cs
+++ DES_ALGORITHM/Form1.cs
@@ -14,13 +14,14 @@ namespace DES_ALGORITHM
 {
     public partial class Form1 : Form
     {
-        
+        static string key;
+        public DES DESalg = DES.Create();
         public Form1()
         {
             InitializeComponent();
             
         }
-        public static string BiteConverterToString(byte [] data)
+        public static string BiteConverterToString(byte[] data)
         {
             byte[] bytes = data;
             Console.WriteLine("The byte array: ");
@@ -32,55 +33,82 @@ namespace DES_ALGORITHM
         }
         public static byte[] StringToBite(string byteString)
         {
-           
+
             // Restore the byte array.
             byte[] newBytes = Convert.FromBase64String(byteString);
             Console.WriteLine("The restored byte array: ");
             Console.WriteLine("   {0}\n", BitConverter.ToString(newBytes));
             return newBytes;
         }
+
         public void button1_Click(object sender, EventArgs e)
         {
+            // ENCRYPT BUTTON
+            //DES DES = DES.Create();
+            //key = generatedKeyTextBox.Text;
+            //text = encryptTextBox.Text;
+            //DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            //vectorTextBox.Text =ASCIIEncoding.ASCII.GetString( DES.IV);
+
+            //byte[] data= EncryptText(text, DES.Key);
+            //encryptedTextResultTextBox.Text = ASCIIEncoding.ASCII.GetString(data);
             try
             {
 
-                DES DESalg = DES.Create();
-                encryptTextBox.Text = "Testuojam gyvenimo prasme 123!";
-                generatedKeyTextBox.Text = "password";
+                
                 string sData = encryptTextBox.Text;
                 string key = generatedKeyTextBox.Text;
+                if(ECBcheckBox.Checked)
+                {
+                    DESalg.Mode = CipherMode.ECB;
+                }
+                if (CBCcheckBox.Checked)
+                {
+                    DESalg.Mode = CipherMode.CBC;
+                }
                 DESalg.Key = ASCIIEncoding.ASCII.GetBytes(key);
-              //  byte[] keyToByte = StringToBite(key);
+
+                //  byte[] keyToByte = StringToBite(key);
                 byte[] Data = EncryptTextToMemory(sData, DESalg.Key, DESalg.IV);
-                string encryptedText = ASCIIEncoding.ASCII.GetString(Data);
-                string IV = BiteConverterToString(DESalg.IV);
-                encryptedTextResultTextBox.Text = encryptedText;
-                vectorTextBox.Text = IV;
-                
+                string encryptedText = Encoding.ASCII.GetString(Data);
+               // string encryptedText = BiteConverterToString(Data);
+                string IV = System.Text.Encoding.ASCII.GetString(DESalg.IV);
+                 //string IV = BiteConverterToString(DESalg.IV);
+                encryptedTextResultTextBox.Text = encryptedText.ToString();
+                vectorTextBox.Text = IV.ToString();
+
             }
             catch (Exception es)
             {
                 Console.WriteLine(es.Message);
             }
+
+
+
         }
         private void decryptButton_Click(object sender, EventArgs e)
         {
-            DES DESalg = DES.Create();
-            string decryptText = decryptTextBox.Text;
-            string key = keyToDecyptherTextBox.Text;
+            // DECRYPT BUTTON
+           
+            if (ECBcheckBox.Checked)
+            {
+                DESalg.Mode = CipherMode.ECB;
+            }
+            if (CBCcheckBox.Checked)
+            {
+                DESalg.Mode = CipherMode.CBC;
+            }
+            string decryptText = encryptTextBox.Text;
+            string key = generatedKeyTextBox.Text;
+            string IV = vectorTextBox.Text;
             DESalg.Key = ASCIIEncoding.ASCII.GetBytes(key);
-            string IV = vectorDecryptTextBox.Text;
-            byte[] dData= StringToBite(decryptText);
-            byte[] keyToByte = StringToBite(key);
-            byte[] IVToByte = StringToBite(IV);
-            string decryptedtext = DecryptTextFromMemory(dData, DESalg.Key, IVToByte);
-            decryptedTextResultBox.Text = decryptedtext;
-
-
+           // DESalg.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            byte[] dData = Encoding.ASCII.GetBytes(decryptText);
+            string decryptedtext = DecryptTextFromMemory(dData, DESalg.Key, DESalg.IV);
+            encryptedTextResultTextBox.Text = decryptedtext;
 
 
         }
-
         public static byte[] EncryptTextToMemory(string Data, byte[] Key, byte[] IV)
         {
             try
@@ -111,9 +139,9 @@ namespace DES_ALGORITHM
                 // Close the streams.
                 cStream.Close();
                 mStream.Close();
-               
+
                 // Return the encrypted buffer.
-                
+
                 return ret;
             }
             catch (CryptographicException e)
@@ -155,10 +183,168 @@ namespace DES_ALGORITHM
                 return null;
             }
         }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            key = generatedKeyTextBox.Text;
+            if(openFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                string source = openFileDialog1.FileName;
+                saveFileDialog1.Filter = "des files |*.des";
+                if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+                {
+                    string destination = saveFileDialog1.FileName;
+                    EncryptFile(source, destination, key);
+                }
+
+            }
+        }
+        private void EncryptFile(string source, string destination , string key)
+        {
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            try
+            {
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform desencrypt = DES.CreateEncryptor();
+                CryptoStream cryptoStream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+                byte[] byteInput = new byte[fsInput.Length - 0];
+                fsInput.Read(byteInput, 0, byteInput.Length);
+                cryptoStream.Write(byteInput, 0, byteInput.Length);
+                cryptoStream.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("Error ENCRYPT");
+                return;
+            }
+            fsInput.Close();
+            fsEncrypted.Close();
+        }
+        private void DecryptFile(string source, string destination, string key)
+        {
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            try
+            {
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform desencrypt = DES.CreateDecryptor();
+                CryptoStream cryptoStream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+                byte[] byteInput = new byte[fsInput.Length - 0];
+                fsInput.Read(byteInput, 0, byteInput.Length);
+                cryptoStream.Write(byteInput, 0, byteInput.Length);
+                cryptoStream.Close();
+
+            }
+            catch
+            {
+                MessageBox.Show("Error Decrpyt");
+                return;
+            }
+            fsInput.Close();
+            fsEncrypted.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            key = generatedKeyTextBox.Text;
+            openFileDialog1.Filter = "des files |*.des";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string source = openFileDialog1.FileName;
+                saveFileDialog1.Filter = "txt files |*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string destination = saveFileDialog1.FileName;
+                    DecryptFile(source, destination, key);
+                }
+
+            }
+        }
+        
+
+        //public static byte[] EncryptTextToMemory(string Data, byte[] Key, byte[] IV)
+        //{
+        //    try
+        //    {
+        //        // Create a MemoryStream.
+        //        MemoryStream mStream = new MemoryStream();
+
+        //        // Create a new DES object.
+        //        DES DESalg = DES.Create();
+
+        //        // Create a CryptoStream using the MemoryStream
+        //        // and the passed key and initialization vector (IV).
+        //        CryptoStream cStream = new CryptoStream(mStream,
+        //            DESalg.CreateEncryptor(Key, IV),
+        //            CryptoStreamMode.Write);
+
+        //        // Convert the passed string to a byte array.
+        //        byte[] toEncrypt = new ASCIIEncoding().GetBytes(Data);
+
+        //        // Write the byte array to the crypto stream and flush it.
+        //        cStream.Write(toEncrypt, 0, toEncrypt.Length);
+        //        cStream.FlushFinalBlock();
+
+        //        // Get an array of bytes from the
+        //        // MemoryStream that holds the
+        //        // encrypted data.
+        //        byte[] ret = mStream.ToArray();
+        //        // Close the streams.
+        //        cStream.Close();
+        //        mStream.Close();
+               
+        //        // Return the encrypted buffer.
+                
+        //        return ret;
+        //    }
+        //    catch (CryptographicException e)
+        //    {
+        //        Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+        //        return null;
+        //    }
+        //}
+        //public static string DecryptTextFromMemory(byte[] Data, byte[] Key, byte[] IV)
+        //{
+        //    try
+        //    {
+        //        // Create a new MemoryStream using the passed
+        //        // array of encrypted data.
+        //        MemoryStream msDecrypt = new MemoryStream(Data);
+
+        //        // Create a new DES object.
+        //        DES DESalg = DES.Create();
+
+        //        // Create a CryptoStream using the MemoryStream
+        //        // and the passed key and initialization vector (IV).
+        //        CryptoStream csDecrypt = new CryptoStream(msDecrypt,
+        //            DESalg.CreateDecryptor(Key, IV),
+        //            CryptoStreamMode.Read);
+
+        //        // Create buffer to hold the decrypted data.
+        //        byte[] fromEncrypt = new byte[Data.Length];
+
+        //        // Read the decrypted data out of the crypto stream
+        //        // and place it into the temporary buffer.
+        //        csDecrypt.Read(fromEncrypt, 0, fromEncrypt.Length);
+
+        //        //Convert the buffer into a string and return it.
+        //        return new ASCIIEncoding().GetString(fromEncrypt);
+        //    }
+        //    catch (CryptographicException e)
+        //    {
+        //        Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+        //        return null;
+        //    }
+        //}
 
         private void generatedKeyTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
+
     }
 }
